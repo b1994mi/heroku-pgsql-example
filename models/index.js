a7c94ecdf1f77e98fd5b0314109cda493fbc8777
b1994mi <b1994mi@gmail.com>
2021-01-27 23:20:41 +0700

    back to using config file sequelizer

--- models/index.js
+++ models/index.js
@@ -4,15 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'production';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-      ssl: true
-  }
-});
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 
 fs
